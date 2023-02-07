15212bf600fb6163c8e8851b6e2743d11331078e
choioonseo <124110679+choioonseo@users.noreply.github.com>
2023-02-07 09:16:57 +0900

    Add files via upload
    230202

---
+++ date0202/out.js
@@ -0,0 +1,22 @@
+function 빨래하다(옷들, 물, 세제, 시간) 
+{
+    console.log("1. 먼지를 털자.")
+    console.log("2. 물 넣기.")
+    console.log("3. 비누 바르기.")
+    console.log("4. 비빈다.")
+    console.log("5. 여러 번 헹군다.")
+    console.log("6. 짠다.")
+    console.log("7. 넌다.")
+}
+
+빨래하다() //함수를 사용, 기능을 사용하는것
+
+
+function 청소하다()
+{
+    console.log("1. 청소도구를 든다")
+    console.log("2. 먼지를 쓴다")
+    console.log("3. 쓰레기를 버린다")
+}
+
+청소하다()
---
+++ date0202/out2.js
@@ -0,0 +1,13 @@
+function 여행하다()
+{
+ console.log("1. 먹는다")
+ console.log("2. 논다")
+ console.log("3. 잔다")
+}
+
+function 쉬다()
+{
+ console.log("1. 침대에 눕는다")
+ console.log("2. 맛있는 거 먹는다")
+ console.log("3. 아무것도 안 한다")
+}
