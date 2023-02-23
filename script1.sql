5319befcbbbb3192b16a436a14f6f8a03a406c35
choioonseo <124110679+choioonseo@users.noreply.github.com>
2023-02-23 17:47:03 +0900

    Create script1.sql
    DB1
    230223

---
+++ script1.sql
@@ -0,0 +1,29 @@
+--테이블명 + 항목명 + 항목에 들어가는 데이터 타입
+ --주석 쓰고 한 줄 띄어줘야 실행됨
+ 
+CREATE TABLE member2 (
+	id varchar2(100),
+	pw varchar2(100),
+	name varchar2(100),
+	tel varchar2(100)
+) 
+
+
+
+CREATE TABLE bbs2 (
+	id varchar2(100),
+	title varchar2(100),
+	content varchar2(100),
+	writer varchar2(100)
+) 
+
+
+
+CREATE TABLE "HR"."PRODUCT2" 
+   (	"ID" VARCHAR2(100), 
+	"NAME" VARCHAR2(100), 
+	"CONTENT" VARCHAR2(100), 
+	"PRICE" NUMBER(38,0)
+   )
+
+
