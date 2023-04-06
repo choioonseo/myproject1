503de049520edec258447412e1b79c591dbe29dd
choioonseo <124110679+choioonseo@users.noreply.github.com>
2023-04-06 17:51:35 +0900

    Add files via upload
    mybatis

---
+++ spring300/src/main/java/com/multi/mvc300/MemberDAO3.java
@@ -0,0 +1,51 @@
+package com.multi.mvc300;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberDAO3 {
+
+	
+//	public int login(MemberVO bag) {
+//	
+//		return result;
+//	}
+//
+//	
+//	public ArrayList<MemberVO> list() {
+//		
+//		return list;
+//
+//	}
+//	
+//	public MemberVO one(String id) {
+//		
+//		return bag;
+//		
+//	}
+//	
+//	
+//	public int delete(String id) {
+//		
+//		return result;
+//		
+//	}
+//
+//	public int update(MemberVO bag) {
+//		
+//		return result;
+//	}
+//	
+	
+	public int insert(MemberVO bag) {
+		
+		return result;
+	}
+}
